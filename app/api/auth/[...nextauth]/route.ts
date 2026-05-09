168d52874a2d2cb51be2e94f426b2c656ab3395f
MKT687 <mkt501881@gmail.com>
2026-05-09 17:34:01 +0900

    first commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      return profile?.email?.endsWith("@stg.nada.ac.jp")
+    },
+  },
+})
+
+export { handler as GET, handler as POST }
---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt"
+
+export async function middleware(req: any) {
+  const token = await getToken({ req })
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/api/auth/signin", req.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ["/"],
+}
