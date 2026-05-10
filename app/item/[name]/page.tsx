c377aa386bd69a56d8bd9f97f9440a8704fefee7
mkt501881-art <mkt501881@gmail.com>
2026-05-10 10:59:58 +0900

    Update page.tsx

--- app/item/[name]/page.tsx
+++ app/item/[name]/page.tsx
@@ -81,7 +81,7 @@ export default function ItemPage() {
     }
 
     try {
-      const res = await fetch("https://あなたのRailwayのURL/request", {
+      const res = await fetch("https://test-discord-production.up.railway.app/request", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
