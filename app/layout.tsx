a6a060c771c399915524a31994a0f017a6fdde26
mkt501881-art <mkt501881@gmail.com>
2026-05-10 11:55:00 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { SessionProvider } from "next-auth/react"
+import { Noto_Sans_JP } from "next/font/google"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
